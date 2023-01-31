7dc9c51b70235dc22d32532a1abf2a46fffbc5a8
Krishbin Paudel <krishbin21@gmail.com>
2023-01-31 19:17:14 +0545

    LSU: defined a simple pipleline lsu
    memifdef.v contains MEM definitions, DEFINITIONS.v updated

--- core/DEFINITIONS.v
+++ core/DEFINITIONS.v
@@ -2,6 +2,8 @@
 `define _DEFINITIONS_V_
 
 // RV32IM INSTRUCTION SET CODES
+`define API_ADDR_WIDTH              32
+`define API_DATA_WIDTH              32
 
 // R-TYPE
 `define INST_R_ALU                  7'b0110011
@@ -105,6 +107,49 @@
     `define BLTU_FUNCT3             3'b110
     `define BGEU_FUNCT3             3'b111
 
-`endif
+// ALU-OPCODES
+// RV32I
+// 0 func7 D5 func3                                            
+// RV32M
+// type(D4,D3) func3
+// RV32F
+// 
+// typetable
+// D4     D3       type
+// 0      f7(D5)    I
+// 1      0         M
+// 1      1         F
+`define ALU_OPCODE_ADD              5'b00000
+`define ALU_OPCODE_SUB              5'b01000
+`define ALU_OPCODE_SLL              5'b00001
+`define ALU_OPCODE_SLT              5'b00010
+`define ALU_OPCODE_SLTU             5'b00011
+`define ALU_OPCODE_XOR              5'b00100
+`define ALU_OPCODE_SRL              5'b00101
+`define ALU_OPCODE_SRA              5'b01101
+`define ALU_OPCODE_OR               5'b00110
+`define ALU_OPCODE_AND              5'b00111
+`define ALU_OPCODE_MUL              5'b10000
+`define ALU_OPCODE_MULH             5'b10001
+`define ALU_OPCODE_MULHSU           5'b10010
+`define ALU_OPCODE_MULHU            5'b10011
+`define ALU_OPCODE_DIV              5'b10100
+`define ALU_OPCODE_DIVU             5'b10101
+`define ALU_OPCODE_REM              5'b10110
+`define ALU_OPCODE_REMU             5'b10110
 
+// LSU-OPCODES
+`define LSU_OPCODE_SIZE             8
+// func3 opcode D6:D2
+`define LSU_OPCODE_LB               8'b00000000
+`define LSU_OPCODE_LH               8'b00100000
+`define LSU_OPCODE_LW               8'b01000000
+`define LSU_OPCODE_LBU              8'b10000000
+`define LSU_OPCODE_LHU              8'b10100000
+`define LSU_OPCODE_SB               8'b00001000
+`define LSU_OPCODE_SH               8'b00101000
+`define LSU_OPCODE_SW               8'b01001000
+`define LSU_OPCODE_NONE             8'b11111111
+
+`endif
 // END OF INSTRUCTION SET CODES
