b8e00c911cdd56e7c21405eb5f7af23afeb7c080
Krishbin Paudel <krishbin21@gmail.com>
2023-02-19 17:54:31 +0545

    LSU: change lsu structure from pipelined to non pipelined
    feedback removed from lsu from previous architecture,
    the low number of pins are a result of this. As we don't need to check
    for status of the pins. Also the response handling of the memory read
    and write operations have been left out. The changes are due to time
    constraints.

---
+++ core/lsu_testbench.v
@@ -0,0 +1,34 @@
+`include "lsu.v"
+
+module rv32im_lsu_testbench();
+
+reg [`API_DATA_WIDTH-1:0] read_from_mem, exu_data_to_mem;
+reg [`API_DATA_WIDTH-1:0] addr;
+wire [`API_DATA_WIDTH-1:0] write_to_mem, mem_data_to_exu, addr_mem_o;
+reg [`LSU_OPCODE_WIDTH-1:0] opcode;
+
+rv32im_lsu a1(
+    .val_memrd_i(read_from_mem),
+    .val_memwr_o(write_to_mem),
+
+    .lsu_opcode_i(opcode),
+    .addr_mem_i(addr),
+    .val_memwr_i(exu_data_to_mem),
+    .val_memrd_o(mem_data_to_exu),
+    .addr_mem_o(addr_mem_o)
+    );
+
+initial begin
+    $dumpfile("lsu.vcd");
+    $dumpvars();
+    $monitor("lsu_opcode=%b, read_from_mem=%b, write_to_mem=%b, exu_data_to_mem=%b, mem_data_to_exu=%b, addr=%b, addr_mem_o=%d \n", opcode, read_from_mem, write_to_mem, exu_data_to_mem, mem_data_to_exu, addr, addr_mem_o);
+    opcode=`LSU_OPCODE_LW; addr=32'd0; exu_data_to_mem=32'd0; read_from_mem=32'd849341;
+    #10
+    opcode=`LSU_OPCODE_LB; addr=32'd0; exu_data_to_mem=32'd0; read_from_mem=32'd849341;
+    #10
+    opcode=`LSU_OPCODE_LH; addr=32'd0; exu_data_to_mem=32'd0; read_from_mem=32'd849341;
+    #10
+    $finish;
+end
+
+endmodule
