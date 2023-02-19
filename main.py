c2c8392bcce781af9666df3feb17303facb13900
Krishbin Paudel <krishbin21@gmail.com>
2023-02-19 19:49:08 +0545

    Compile and test helper: python runner
    follow a standard to name the testbenches as
    tb_<module>.v
    
    need to place these in the folder tb/ inside the root directory
    
    the include in modules inside the core directory should be relative to
    core directory and not to the root directory.
    
    also you don't need to provide path to dump the waveform(.vcd) file
    just name them as their respective module name with the vcd extension
    Example:
    For alu.v module in core
    inside alu.v:
    ```verilog
    `include "def.v"
    ```
    def.v resides with alu.v in core directory.
    i.e
        core/
            - alu.v
            - def.v
    For testbench of alu.v:
    it exists in
        tb/
            - tb_alu.v
    take care of the file naming format
    inside tb_alu.v:
    ```
    $dumpfile("alu.vcd")
    ```
    
    After running this script from the root folder you will have:
    compiled verilog files in {ROOT_DIR}/bin folder and waveform in
    {ROOT_DIR}/waveform folder.

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def sanitycheck():
+    # check if the script is running in the correct directory
+    if os.path.basename(os.getcwd()) != 'apicore':
+        print('Please run this script from the apicore directory')
+        sys.exit(1)
+
+    # check if iverilog is installed
+    if os.system('iverilog -V') != 0:
+        print('Please install iverilog')
+        sys.exit(1)
+
+def listfiles():
+    # list all files in the directory 
+    files = os.listdir("tb")
+    if len(files) == 0 or os.path.exists(f'{ROOT_DIR}/tb') == False:
+        print('No testbench files found')
+        sys.exit(1)
+    # filter out all files that are not .v files
+    files = [f for f in files if f.endswith('.v')]
+    # filter out all files that are not testbench files
+    files = [f for f in files if f.startswith('tb_')]
+    # sort the files
+    files.sort()
+    # return the list of files
+    return files
+
+def runtestbench(file):
+    # check if the bin directory exists
+    if not os.path.exists(f'{ROOT_DIR}/bin'):
+        os.makedirs(f'{ROOT_DIR}/bin')
+    # check if the waveform directory exists
+    if not os.path.exists(f'{ROOT_DIR}/waveform'):
+        os.makedirs(f'{ROOT_DIR}/waveform')
+    # run the testbench
+    os.system(f'iverilog -grelative-include -o {ROOT_DIR}/bin/{file[:-2]} {ROOT_DIR}/tb/{file}')
+    os.system(f'cd {ROOT_DIR}/waveform && vvp {ROOT_DIR}/bin/{file[:-2]}')
+
+def main():
+    print('Running sanity check...')
+    sanitycheck()
+    print('Sanity check passed')
+    print('Listing files...')
+    files = listfiles()
+    print('Files listed')
+    print('Running testbenches...')
+    for file in files:
+        print(f'Running testbench {file}')
+        runtestbench(file)
+    print('Testbenches ran')
+
+if __name__ == '__main__':
+    main()
