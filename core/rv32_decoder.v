c6d41ab7ab831ad9cfcf491aebf66855549a642c
Krishbin Paudel <krishbin21@gmail.com>
2023-02-22 19:30:38 +0545

    CU and DECODE unit: implementation of cu along with the decode
    implementation of system instruction ( csr, fence, ecall, ebreak ) are
    left. so this is untested but verified with verilog with no syntax
    errors.

--- core/rv32_decoder.v
+++ core/rv32_decoder.v
@@ -1,20 +1,43 @@
-`include "../core/DEFINITIONS.v"
+`include "DEFINITIONS.v"
 
-module decoder(
+module rv32im_decoder_and_cu(
     input clk,
-    input [31:0] instruction,
+    input [`API_DATA_WIDTH-1:0] instruction,
 
-    output [31:0] decoded_value,
-    output [4:0] rs1,
-    output [4:0] rs2,
-    output [4:0] rd,
-    output [6:0] opcode,
-    output [2:0] funct3,
-    output [6:0] funct7,
-    output shift_la_sel,        // logical or arithmetic shift select
-    output [7:0] pred_succ,     // Fence instruction 
-    output e_call_break,
-    output trap
+    //----------------------------------------------------------//
+    // Execution Unit 
+    //----------------------------------------------------------//
+    output reg [`DATA_ORIGIN_WIDTH-1:0] data_origin_o,
+    output reg [`DATA_TARGET_WIDTH-1:0] data_target_o,
+    output reg [`API_DATA_WIDTH-1:0] imm_o,
+    // register file
+    output reg [`API_REGISTER_ADDR_WIDTH-1:0] rs1_addr_o,
+    output reg [`API_REGISTER_ADDR_WIDTH-1:0] rs2_addr_o,
+    output reg [`API_REGISTER_ADDR_WIDTH-1:0] rd_addr_o,
+    // alu
+    output reg [`ALU_OPCODE_WIDTH-1:0] alu_opcode_o,
+    output reg [`LSU_OPCODE_WIDTH-1:0] lsu_opcode_o,
+
+    //branch
+    output reg [`BR_OPCODE_WIDTH-1:0] br_opcode_o,
+    output reg is_branch_o,
+    output reg is_condition_o,
+    //----------------------------------------------------------//
+
+    //----------------------------------------------------------//
+    //CSR Unit
+    output reg [`CSR_OPCODE_WIDTH-1:0] csr_opcode_o,
+    output reg [`CSR_WIDTH-1:0] csr_addr_o,
+    output reg [`API_DATA_WIDTH-1:0] csr_data_o,
+
+    // memory
+    output reg mem_w_o,
+    output reg reg_w_o
+
+    // output shift_la_sel,        // logical or arithmetic shift select
+    // output [7:0] pred_succ,     // Fence instruction 
+    // output e_call_break,
+    // output trap
 );
 
     localparam  R_TYPE = 3'b000,
@@ -25,170 +48,258 @@ module decoder(
                 B_TYPE = 3'b111;
 
 
-    reg [31:0] decoded_value_reg, decoded_value_next;
-    reg [4:0] rs1_reg, rs2_reg, rs1_next, rs2_next, rd_reg, rd_next;
-    reg [6:0] opcode_reg, opcode_next, funct7_reg, funct7_next;
-    reg [2:0] funct3_reg, funct3_next;
-    reg shift_la_sel_reg, e_call_break_reg, trap_reg;
-    reg [7:0] pred_succ_reg;
+    reg [6:0] opcode;
+    reg [6:0] func7;
+    reg [2:0] func3;
+    // r type
+    reg [4:0] rs1,rs2,rd;
 
-    // assign output
-    assign opcode = opcode_reg;
-    assign funct3 = funct3_reg;
-    assign funct7 = funct7_reg;
-    assign decoded_value = decoded_value_reg;
-    assign rs1 = rs1_reg;
-    assign rs2 = rs2_reg;
-    assign rd = rd_reg;
-    assign shift_la_sel = shift_la_sel_reg;
-    assign e_call_break = e_call_break_reg;
-    assign trap = trap_reg;
-    assign pred_succ = pred_succ_reg;
+    // u type
+    reg [19:0] imm_20;
+
+    // i type
+    reg [11:0] imm_12;
+
+    // s type
+    reg [11:0] imm_12_s;
+
+    // b type
+    reg [11:0] imm_12_b;
+
+    // j type
+    reg [19:0] imm_20_j;
 
     wire [2:0] instruction_type;
-    assign instruction_type =   (instruction[6:0] == `INST_R_ALU )     ? R_TYPE :
-                                (
-                                instruction[6:0]  == `INST_I_ALU   ||
-                                instruction[6:0]  == `INST_I_JALR  ||
-                                instruction[6:0]  == `INST_I_LOAD  ||
-                                instruction[6:0]  == `INST_I_SHIFT ||
-                                instruction[6:0]  == `INST_S_STORE ||
-                                instruction[6:0]  == `INST_I_CSR   ||
-                                instruction[6:0]  == `INST_I_FENCE ||
-                                instruction[6:0]  == `INST_I_CALL_N_BREAK
-                                )                                      ? I_TYPE :
-                                ( 
-                                instruction[6:0]  == `INST_U_LUI   ||
-                                instruction[6:0]  == `INST_U_AUIPC
-                                )                                      ? U_TYPE :
-                                ( instruction[6:0]  == `INST_J_JAL)    ? J_TYPE : 
-                                ( instruction[6:0]  == `INST_B_BRANCH) ? B_TYPE : 3'bz;
-
-    // perform update of decoded information at posedge of clock only
-    always @(posedge clk) begin
-        decoded_value_reg <= decoded_value_next;
-        rs1_reg <= rs1_next;
-        rs2_reg <= rs2_next;
-        rd_reg  <= rd_next;
-        opcode_reg <= opcode_next;
-        funct3_reg <= funct3_next;
-        funct7_reg <= funct7_next;
-        shift_la_sel_reg <= (opcode_reg == `INST_I_SHIFT && funct7_reg !== 7'bz) ? instruction[30] : 1'bz;
-        e_call_break_reg <= (opcode_reg == `INST_I_CALL_N_BREAK && instruction[14:12] == 3'b0) ? instruction[20] : 1'bz;
-        trap_reg <= (rd_next === 5'b00000) ? 1'b1 : 1'b0;
-        pred_succ_reg <= (opcode_reg == `INST_I_FENCE) ? ( (instruction[14:12] == 3'b000) ? instruction[27:20] : 8'b0) : 8'bz;
-    end
 
     // decode next available instruction
     always @(*) begin
 
-        opcode_next = instruction[6:0];
+        mem_w_o = 1'b0;
+        reg_w_o = 1'b0;
+        is_branch_o = 1'b0;
+        is_condition_o = 1'b0;
+        data_origin_o = {`DATA_ORIGIN_WIDTH{1'b0}};
+        data_target_o = {`DATA_TARGET_WIDTH{1'b0}};
+        imm_o = {`API_DATA_WIDTH{1'b0}};
+        rs1_addr_o = {`API_REGISTER_ADDR_WIDTH{1'b0}};
+        rs2_addr_o = {`API_REGISTER_ADDR_WIDTH{1'b0}};
+        rd_addr_o = {`API_REGISTER_ADDR_WIDTH{1'b0}};
+        alu_opcode_o = {`ALU_OPCODE_WIDTH{1'b0}};
+        lsu_opcode_o = {`LSU_OPCODE_WIDTH{1'b0}};
+        br_opcode_o = {`BR_OPCODE_WIDTH{1'b0}};
+        csr_opcode_o = {`CSR_OPCODE_WIDTH{1'b0}};
+        csr_addr_o = {`CSR_WIDTH{1'b0}};
+        csr_data_o = {`API_DATA_WIDTH{1'b0}};
         
-        case(instruction_type)
-            R_TYPE: begin
-                decoded_value_next = 32'bz; // no information to be decoded
-                rs1_next = instruction[19:15];
-                rs2_next = instruction[24:20];
-                rd_next = instruction[11:7];
-                funct3_next = instruction[14:12];
-                funct7_next = instruction[31:25];
+        opcode = instruction[6:0];
+        func7 = instruction[31:25];
+        func3 = instruction[14:12];
+        rs1 = instruction[19:15];
+        rs2 = instruction[24:20];
+        rd = instruction[11:7];
+        imm_12 = instruction[31:20];
+        imm_20 = instruction[31:12];
+        imm_20_j = { instruction[31], instruction[19:12], instruction[20], instruction[30:21] };
+        imm_12_b = { instruction[31], instruction[7], instruction[30:25], instruction[11:8] };
+        imm_12_s = { instruction[31:25], instruction[11:7] };
+
+        case(opcode)
+            `INST_R_ALU: begin
+                data_origin_o = `DATA_ORIGIN_REGISTER; // provide data to alu
+                reg_w_o = 1'b1;
+                rs1_addr_o = rs1;
+                rs2_addr_o = rs2;
+                rd_addr_o = rd;
+                case (func3)
+                    `ADD_SUB_FUNCT3: alu_opcode_o = func7[5] == 1'b1 ? `ALU_OPCODE_SUB : `ALU_OPCODE_ADD;
+                    `SLL_FUNCT3: alu_opcode_o = `ALU_OPCODE_SLL;
+                    `SLT_FUNCT3: alu_opcode_o = `ALU_OPCODE_SLT;
+                    `SLTU_FUNCT3: alu_opcode_o = `ALU_OPCODE_SLTU;
+                    `XOR_FUNCT3: alu_opcode_o = `ALU_OPCODE_XOR;
+                    `SRL_SRA_FUNCT3: alu_opcode_o = func7[5] == 1'b1 ? `ALU_OPCODE_SRA : `ALU_OPCODE_SRL;
+                    `OR_FUNCT3: alu_opcode_o = `ALU_OPCODE_OR;
+                    `AND_FUNCT3: alu_opcode_o = `ALU_OPCODE_AND;
+                endcase
+                data_target_o = `DATA_TARGET_ALU; // use data from alu
             end
 
-            I_TYPE: begin
-                if (opcode_next == `INST_I_ALU || opcode_next == `INST_I_JALR || opcode_next == `INST_I_LOAD) begin
-                    decoded_value_next = { {21{instruction[31]}}, instruction[30:20]};
-                    rs1_next = instruction[19:15];
-                    rs2_next = 5'bz;
-                    rd_next = instruction[11:7];
-                    funct3_next = instruction[14:12];
-                    funct7_next = 7'bz;
-                end
-                else if (opcode_next == `INST_I_SHIFT) begin
-                    decoded_value_next = { {27{instruction[31]}}, instruction[24:20]}; 
-                    rs1_next = instruction[19:15];
-                    rs2_next = instruction[24:20];
-                    rd_next = instruction[11:7];
-                    funct3_next = instruction[14:12];
-                    funct7_next = instruction[31:25];    
-                end
-                else if (opcode_next == `INST_I_CSR && instruction[14:12] !== 3'b0) begin
-                    if ( instruction[14] )  begin// instruction[14] differentiates imm. '1' -> imm : CSRRSI, CSRRWI, CSRRCI
-                        decoded_value_next = { 27'b0 + instruction[19:15]};
+            `INST_I_ALU: begin
+                data_origin_o = `DATA_ORIGIN_RS1_IMM; // provide data to alu
+                imm_o = { {20{imm_12[11]}}, imm_12 };
+                reg_w_o = 1'b1;
+                rs1_addr_o = rs1;
+                rd_addr_o = rd;
+                case (func3)
+                    `ADD_SUB_FUNCT3: alu_opcode_o = func7[5] == 1'b1 ? `ALU_OPCODE_SUB : `ALU_OPCODE_ADD;
+                    `SLL_FUNCT3: alu_opcode_o = `ALU_OPCODE_SLL;
+                    `SLT_FUNCT3: alu_opcode_o = `ALU_OPCODE_SLT;
+                    `SLTU_FUNCT3: alu_opcode_o = `ALU_OPCODE_SLTU;
+                    `XOR_FUNCT3: alu_opcode_o = `ALU_OPCODE_XOR;
+                    `SRL_SRA_FUNCT3: alu_opcode_o = func7[5] == 1'b1 ? `ALU_OPCODE_SRA : `ALU_OPCODE_SRL;
+                    `OR_FUNCT3: alu_opcode_o = `ALU_OPCODE_OR;
+                    `AND_FUNCT3: alu_opcode_o = `ALU_OPCODE_AND;
+                endcase
+                data_target_o = `DATA_TARGET_ALU; // use data from alu
+            end
+
+            `INST_I_LOAD: begin
+                reg_w_o = 1'b1;
+                rs1_addr_o = rs1;
+                rd_addr_o = rd;
+                alu_opcode_o = `ALU_OPCODE_ADD; // add imm to rs1
+                data_origin_o = `DATA_ORIGIN_RS1_IMM; // provide data to alu
+                imm_o = { {20{imm_12[11]}}, imm_12 };
+                case (func3)
+                    `LB_FUNCT3: lsu_opcode_o = `LSU_OPCODE_LB;
+                    `LH_FUNCT3: lsu_opcode_o = `LSU_OPCODE_LH;
+                    `LW_FUNCT3: lsu_opcode_o = `LSU_OPCODE_LW;
+                    `LBU_FUNCT3: lsu_opcode_o = `LSU_OPCODE_LBU;
+                    `LHU_FUNCT3: lsu_opcode_o = `LSU_OPCODE_LHU;
+                    default: lsu_opcode_o = `LSU_OPCODE_NONE;
+                endcase
+                data_target_o = `DATA_TARGET_MEM; // use data from memory
+            end
+
+            `INST_I_JALR: begin
+                is_branch_o = 1'b1;
+                data_origin_o = `DATA_ORIGIN_RS1_IMM; // provide data to alu
+                rs1_addr_o = rs1;
+                alu_opcode_o = `ALU_OPCODE_ADD; // add imm to rs1
+                imm_o = { {20{imm_12[11]}}, imm_12 };
+                reg_w_o = 1'b1;
+                rd_addr_o = rd;
+                data_target_o = `DATA_TARGET_PC; // use data from pc provided by br
+            end
+
+            `INST_I_SYS: begin
+                reg_w_o = 1'b1;
+                rd_addr_o = rd;
+                csr_addr_o = imm_12;
+                case(func3)
+                    `ECALL_EBREAK_FUNCT3: ;
+                    `CSRRW_FUNCT3: begin
+                        csr_opcode_o = `CSRRW;
+                        csr_data_o = rs1;
                     end
-                    else begin
-                        decoded_value_next = { 20'b0 + instruction[31:20]};
+                    `CSRRS_FUNCT3: begin
+                        csr_opcode_o = `CSRRS;
+                        csr_data_o = rs1;
                     end
-                end
-                else if (opcode_next == `INST_I_CALL_N_BREAK && instruction[14:12] == 3'b0) begin
-                    rd_next = 5'b0;
-                    funct3_next = 3'b0;
-                    funct7_next = 7'bz;
-                end
-                else if (opcode_next == `INST_I_FENCE) begin
-                    funct3_next = instruction[14:12];
-                end
+                    `CSRRC_FUNCT3: begin
+                        csr_opcode_o = `CSRRC;
+                        csr_data_o = rs1;
+                    end
+                    `CSRRWI_FUNCT3: begin
+                        csr_opcode_o = `CSRRWI;
+                        csr_data_o = imm_12;
+                    end
+                    `CSRRSI_FUNCT3: begin
+                        csr_opcode_o = `CSRRSI;
+                        csr_data_o = imm_12;
+                    end
+                    `CSRRCI_FUNCT3: begin
+                        csr_opcode_o = `CSRRCI;
+                        csr_data_o = imm_12;
+                    end
+                endcase
             end
 
-            S_TYPE: begin
-                decoded_value_next = { {21{instruction[31]}}, instruction[30:25], instruction[11:7]};
-                rs1_next = instruction[19:15];
-                rs2_next = instruction[24:20];
-                rd_next = 5'bz;
-                funct3_next = instruction[14:12];
-                funct7_next = 7'bz;
+            `INST_I_FENCE: begin
             end
 
-            U_TYPE: begin
-                decoded_value_next = {instruction[31:12], 12'b0};
-                rs1_next = 5'bz;
-                rs2_next = 5'bz;
-                rd_next = instruction[11:7];
-                funct3_next = 3'bz;
-                funct7_next = 7'bz;
+            `INST_S_STORE: begin
+                data_origin_o = `DATA_ORIGIN_RS1_IMM;
+                rs1_addr_o = rs1;
+                rs2_addr_o = rs2;
+                mem_w_o = 1'b1;
+                alu_opcode_o = `ALU_OPCODE_ADD;
+                imm_o = { {20{imm_12_s[11]}}, imm_12_s };
+                case(func3)
+                    `SB_FUNCT3: lsu_opcode_o = `LSU_OPCODE_SB;
+                    `SH_FUNCT3: lsu_opcode_o = `LSU_OPCODE_SH;
+                    `SW_FUNCT3: lsu_opcode_o = `LSU_OPCODE_SW;
+                endcase
             end
 
-            J_TYPE: begin
-                decoded_value_next = { {12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};
-                rs1_next = 5'bz;
-                rs2_next = 5'bz;
-                rd_next = instruction[11:7];
-                funct3_next = 3'bz;
-                funct7_next = 7'bz;
+            `INST_U_AUIPC: begin
+                data_origin_o = `DATA_ORIGIN_PC_IMM;
+                imm_o = { imm_20, {12{1'b0}} };
+                reg_w_o = 1'b1;
+                rd_addr_o = rd;
+                alu_opcode_o = `ALU_OPCODE_ADD;
+                data_target_o = `DATA_TARGET_ALU;
             end
 
-            B_TYPE: begin
-                decoded_value_next = { {20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
-                rs1_next = instruction[19:15];
-                rs2_next = instruction[24:20];
-                rd_next = 5'bz;
-                funct3_next = instruction[14:12];
-                funct7_next = instruction[31:25];
+            `INST_U_LUI: begin
+                data_origin_o = `DATA_ORIGIN_RS1_IMM;
+                imm_o = { imm_20, {12{1'b0}} };
+                reg_w_o = 1'b1;
+                rd_addr_o = rd;
+                alu_opcode_o = `ALU_OPCODE_ADD;
+                rs1_addr_o = {`API_REGISTER_ADDR_WIDTH{1'b0}};
+                data_target_o = `DATA_TARGET_ALU;
+            end
+
+            `INST_J_JAL: begin
+                is_branch_o = 1'b1;
+                data_origin_o = `DATA_ORIGIN_PC_IMM;
+                imm_o = { {11{imm_20_j[19]}}, imm_20_j, {1'b0} };
+                reg_w_o = 1'b1;
+                rd_addr_o = rd;
+                alu_opcode_o = `ALU_OPCODE_ADD;
+                data_target_o = `DATA_TARGET_PC;
+            end
+
+            `INST_B_BRANCH: begin
+                is_branch_o = 1'b1;
+                is_condition_o = 1'b1;
+                data_origin_o = `DATA_ORIGIN_REGISTER;
+                imm_o = { {19{1'b0}}, imm_12_b , {1'b0} };
+
+                rs1_addr_o = rs1;
+                rs2_addr_o = rs2;
+                case(func3)
+                    `BEQ_FUNCT3: begin
+                        alu_opcode_o = `ALU_OPCODE_SUB;
+                        br_opcode_o = `BR_OPCODE_BEQ;
+                    end
+                    `BNE_FUNCT3: begin
+                        alu_opcode_o = `ALU_OPCODE_SUB;
+                        br_opcode_o = `BR_OPCODE_BNE;
+                    end
+                    `BLT_FUNCT3: begin
+                        alu_opcode_o = `ALU_OPCODE_SLT;
+                        br_opcode_o = `BR_OPCODE_BLT;
+                    end
+                    `BGE_FUNCT3: begin
+                        alu_opcode_o = `ALU_OPCODE_SLT;
+                        br_opcode_o = `BR_OPCODE_BGE;
+                    end
+                    `BLTU_FUNCT3: begin
+                        alu_opcode_o = `ALU_OPCODE_SLTU;
+                        br_opcode_o = `BR_OPCODE_BLT;
+                    end
+                    `BGEU_FUNCT3: begin
+                        alu_opcode_o = `ALU_OPCODE_SLTU;
+                        br_opcode_o = `BR_OPCODE_BGE;
+                    end
+                endcase
             end
 
             default: begin
-                decoded_value_next = 32'bz;
-                rs1_next = 5'b0;
-                rs2_next = 5'bz;
-                rd_next = 5'b0;
-                opcode_next = `INST_I_ALU;
-                funct3_next = `ADDI_FUNCT3;
-                funct7_next = 7'b0;
+                rs1_addr_o = 5'b0;
+                rd_addr_o = 5'b0;
+                alu_opcode_o = `ALU_OPCODE_ADD;
+                data_origin_o = `DATA_ORIGIN_REGISTER;
+                imm_o = 32'b0;
+                reg_w_o = 1'b0;
+                is_branch_o = 1'b0;
+                is_condition_o = 1'b0;
+                data_target_o = `DATA_TARGET_ALU;
+                mem_w_o = 1'b0;
+                lsu_opcode_o = `LSU_OPCODE_NONE;
             end
         endcase
-    end
-
-    initial begin
-        opcode_reg = 7'b0;
-        funct3_reg = 3'b0;
-        funct7_reg = 3'b0;
-        decoded_value_reg = 32'bz;
-        rs1_reg = 5'bz;
-        rs2_reg = 5'bz;
-        rd_reg = 5'bz;  
-        trap_reg = 1'b0;
-        shift_la_sel_reg = 1'bz;
-        e_call_break_reg = 1'bz;
-        pred_succ_reg = 8'bz;
     end
 
 endmodule
