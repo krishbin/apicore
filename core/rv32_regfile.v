5126290d6ad0d6f01d487974a377e3bc88d59460
Krishbin Paudel <krishbin21@gmail.com>
2023-02-22 19:05:57 +0545

    regfile: removed simulation defaults (initial block)
    reimplemented the regfile to be able to synthesize in hardware

--- core/rv32_regfile.v
+++ core/rv32_regfile.v
@@ -1,54 +1,35 @@
 // APICORE : RV32I 
 // Date    : 2023-01-23
 
-module regfile(
-    input clk, 
-    input reset_n,
-    input [4:0] rs1_address, 
-    input [4:0] rs2_address, 
-    input [4:0] rd_address,
-    input [31:0] rd_value,
-    output reg [31:0] rs1_value_o,
-    output reg [31:0] rs2_value_o
+module rv32im_regfile(
+    input clk_i, 
+    input rst_n_i,
+    input we_i,
+    input [`API_REGISTER_ADDR_WIDTH-1:0] rs1_addr_i, 
+    input [`API_REGISTER_ADDR_WIDTH-1:0] rs2_addr_i, 
+    input [`API_REGISTER_ADDR_WIDTH-1:0] rd_addr_i,
+    input [`API_REGISTER_WIDTH-1:0] val_rd_i,
+    output [`API_REGISTER_WIDTH-1:0] val_rs1_o,
+    output [`API_REGISTER_WIDTH-1:0] val_rs2_o
 );
 
-    // to hold previous register value before rd_value modifies any register
-    reg [31:0] rs1_value_reg, rs2_value_reg;
-
     // continuous memory implementation for register file: 32 unique locations of 32-bit each
-    reg [31:0] registerArray [0:31];
-
+    reg [`API_REGISTER_WIDTH-1:0] registerArray [0:`API_REGISTER_COUNT-1];
     integer i;
-    always @(posedge clk or negedge reset_n) begin
-        if ( ~reset_n ) begin
-            for (i=0;i<32;i=i+1) 
+    always @(posedge clk_i or negedge rst_n_i) begin
+        
+        if ( ~rst_n_i ) begin
+
+            for (i=0;i<`API_REGISTER_COUNT;i=i+1)
                 registerArray[i] <= 'b0;
         end
         else begin
-            if (rd_address !== 5'b0)
-                registerArray[rd_address] = rd_value;
-            rs1_value_o <= rs1_value_reg;
-            rs2_value_o <= rs2_value_reg;
+            if ( (rd_addr_i !== 5'b0) && we_i )
+                registerArray[rd_addr_i] = val_rd_i;
         end
     end
 
-    always @(*) begin
-        if ( ~reset_n ) begin
-            rs1_value_reg = 'bz;
-            rs2_value_reg = 'bz;
-        end
-        else begin
-            rs1_value_reg = (rs1_address !== 5'bz) ? registerArray[rs1_address] : 'bz;
-            rs2_value_reg = (rs2_address !== 5'bz) ? registerArray[rs2_address] : 'bz;
-        end
-    end
-
-    initial begin
-        for (i=0;i<32;i=i+1) 
-            registerArray[i] <= 'b0;
-        rs1_value_o = 'bz;
-        rs2_value_o = 'bz;
-    end
-
+    assign val_rs1_o = (rs1_addr_i == 5'b0) ? registerArray[rs1_addr_i] : { `API_REGISTER_WIDTH{1'b0} };
+    assign val_rs2_o = (rs2_addr_i == 5'b0) ? registerArray[rs2_addr_i] : { `API_REGISTER_WIDTH{1'b0} };
 
 endmodule
